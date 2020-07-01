97600549662e3bd0aab3e56451fce1432d32528d
Bruna Souza <bruna.ssouza@outlook.com.br>
2020-06-30 22:18:26 -0300

    subindo primeira parte do pokemon

--- Desafio_Pokemon/pokemon.js
+++ Desafio_Pokemon/pokemon.js
@@ -1,29 +1,42 @@
-const axios = require('axios');
+const axios = require("axios");
 user = require("readline-sync");
+var fs = require("fs");
 
-function mostraDadosPokemon(){
-    var pokemonDigitado= user.question("Digite o nome ou o ID do pokemon : ");
-    axios.get("https://pokeapi.co/api/v2/pokemon/"+pokemonDigitado).then(function(response) {
-        //console.log(`${response.data.forms[0].name}` + "\n" + `${response.data.types[0].type.name}`);
-        var json = "";
-        response.data.forms.forEach(element => {
-            json = json.concat(`{"name":"${element.name}",`);
-            console.log(json);
-            console.log("Name: " + element.name);
-        });
-        console.log("Types:");
-        json = json.concat(`"tipos:"[`)
-        response.data.types.forEach(element => {
-            json = json.concat(`"tipo:{"${element.type.name}"}`)
-            console.log(element.type.name);
-        });
-        json = json.concat("]");
-        console.log(json)
-        console.log("Abilities: ");
-        response.data.abilities.forEach(element => {
-            console.log(element.ability.name);
-        });
-    })
-} 
+function mostraDadosPokemon() {
+  var pokemonDigitado = user
+    .question("Por favor, digite o nome ou o ID do pokemon desejado: ")
+    .toLowerCase();
+
+  axios
+    .get("https://pokeapi.co/api/v2/pokemon/" + pokemonDigitado)
+    .then(function (response) {
+      console.log(
+        `Nome: ${response.data.forms[0].name}` +
+          "\n" +
+          `Tipo: ${response.data.types[0].type.name}`
+      );
+      console.log("Lista de Habilidades:");
+
+      response.data.abilities.forEach((element) => {
+        console.log(element.ability.name);
+      });
+      montaJsonPokedex(response);
+    });
+}
+
+function montaJsonPokedex(response) {
+  var listaHabilidades = [];
+
+  response.data.abilities.forEach((element) => {
+    listaHabilidades.push(element.ability.name);
+  });
+
+  var responseString = {
+    nome: response.data.forms[0].name,
+    tipo: response.data.types[0].type.name,
+    habilidades: listaHabilidades,
+  };
+  fs.writeFileSync("./pokedex.json", JSON.stringify(responseString));
+}
 
 mostraDadosPokemon();
